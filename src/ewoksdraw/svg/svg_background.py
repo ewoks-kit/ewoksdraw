0516e4a528f813682684286aaaa7c02685a8cf1a
Ludovic Broche <ludovic.broche@esrf.fr>
2025-07-29 11:04:22 +0200

    Small Refactors

--- src/ewoksdraw/svg/svg_background.py
+++ src/ewoksdraw/svg/svg_background.py
@@ -6,10 +6,6 @@ class SvgBackground(SvgElement):
     Represents a background rectangle element in SVG.
     Color can be set using the ./css_styles/css_background.css
     style.
-
-
-    :param width: The width of the background rectangle.
-    :param height: The height of the background rectangle.
     """
 
     def __init__(self, width: float, height: float):
